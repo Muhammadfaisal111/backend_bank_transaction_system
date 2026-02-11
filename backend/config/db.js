6f89a29c181516bbb05e1f101d6ad182582cf442
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-02-12 00:06:45 +0500

    server and database connect using mongodb atlass and compass

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+function connectDB() {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB Connected"))
+    .catch((err) => {
+      console.error(err);
+      process.exit(1);
+    });
+}
+
+module.exports = connectDB;
