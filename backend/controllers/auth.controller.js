fb7b717a8b2b4813fa18b6f4d6e24fd27687af0e
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-02-12 15:25:01 +0500

    login api complete and test with postman

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+
+// Register
 async function registerUserController(req, res) {
-  console.log(req.body);
   const { email, name, password } = req.body;
   const isExist = await userModel.findOne({ email });
   if (isExist) {
@@ -22,6 +23,28 @@ async function registerUserController(req, res) {
   });
 }
 
+// Login
+async function loginUserController(req, res) {
+  const { email, password } = req.body;
+  const user = await userModel.findOne({ email }).select("+password");
+  if (!user) {
+    return res.status(404).json({ message: "email or password is invalid" });
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return res.status(404).json({ message: "email or password is invalid" });
+  }
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  res.cookie("token", token);
+  res.status(201).json({
+    user: { _id: user._id, email: user.email, name: user.name },
+    token,
+  });
+}
+// Export outside functions
 module.exports = {
   registerUserController,
+  loginUserController,
 };
--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,6 +1,10 @@
 const express = require("express");
-const  {registerUserController} = require("../controllers/auth.controller");
+const {
+  registerUserController,
+  loginUserController,
+} = require("../controllers/auth.controller");
 const router = express.Router();
 router.post("/register", registerUserController);
+router.post("/login", loginUserController);
 
 module.exports = router;
