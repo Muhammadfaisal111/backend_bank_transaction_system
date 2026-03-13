44a32f724671ae0e04a32f404468efc587f30272
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-03-13 14:24:26 +0500

    auth middleware and account router

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const authRouter = require("../routes/user.router");
+
 
 app.use(express.json());
 app.use(cookieParser());
+
+const authRouter = require("../routes/user.router");
+const accountRouter = require("../routes/account.router");
+
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
 
 module.exports = app;
