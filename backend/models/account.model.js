f5e27ba826c09953b7825fdde54907f7014c7211
Muhammad faisal <muhammadfaisal7864309@gmail.com>
2026-03-13 10:51:19 +0500

    account model

---
+++ backend/models/account.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const accountSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "account must belong to a user"],
+      index: true,
+    },
+    status: {
+      enum: {
+        values: ["active", "inactive", "frozen"],
+        message: "Status must be either active, inactive or frozen",
+      },
+    },
+    currency: {
+      type: String,
+      required: [true, "Currency is required"],
+      default: "PKR",
+    },
+  },
+  { timestamps: true }
+);
+
+accountSchema.index({ user: 1 }, { Schema: 1 });
+
+const accountModel = mongoose.model("Account", accountSchema);
+module.exports = accountModel;
